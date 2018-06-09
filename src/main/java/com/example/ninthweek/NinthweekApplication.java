b086cff29bb10c90f7e6264291e810972abd9bf2
wan000si <576529493@qq.com>
2018-06-09 21:59:11 +0800

    first commit

---
+++ src/main/java/com/example/ninthweek/NinthweekApplication.java
@@ -0,0 +1,12 @@
+package com.example.ninthweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NinthweekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NinthweekApplication.class, args);
+	}
+}
